fbd8898b7ff5ca425189eaeefc1025d8fc600065
Tcgogo <1812497229@qq.com>
2020-12-06 21:57:26 +0800

    完成下拉菜单功能

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" @click.prevent="btnClick">{{ title }}</button>
+    <ul class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
+    <slot></slot>
+  </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Dropdown",
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    const isOpen = ref(false);
+    const btnClick = () => {
+      isOpen.value = !isOpen.value;
+    }
+
+    return {
+      isOpen,
+      btnClick
+    }
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/DropdownItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li class="dropdown-option isdisabled" :class="{'isdisabled': disabled}">
+    <slot></slot>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "DropdownItem",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.isdisabled{
+  color: rgb(247, 0, 0);
+  pointer-events: none;
+  background: transparent;
+}
+</style>
