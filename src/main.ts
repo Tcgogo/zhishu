29b7252c94b075d940a77d9ae102635468c6e686
Tcgogo <1812497229@qq.com>
2020-12-11 01:03:54 +0800

    使用axios拦截器实现简单的loading效果

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import store from "./store"
 import axios from "axios";
 
 axios.defaults.baseURL = 'http://localhost:8080/api/';
+axios.interceptors.request.use(config => {
+  store.commit("setLoading", true);
+  return config;
+})
+axios.interceptors.response.use(config => {
+  store.commit("setLoading", false);
+  return config;
+})
 const app = createApp(App);
 app.use(router);
 app.use(store);
