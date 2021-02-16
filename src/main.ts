8c48ff1edf52eb16917c873eb3d24187d3982c31
Tcgogo <1812497229@qq.com>
2021-02-16 17:09:53 +0800

    接口更改

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,29 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:8080/api/" : "https://bird.ioliu.cn/v1/?url=http://api.vikingship.xyz/api/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/api/"
+    : "http://api.vikingship.xyz/api/";
 axios.interceptors.request.use((config) => {
   store.commit("setLoading", true);
-  store.commit("setError", {status: false, message: ""});
+  store.commit("setError", { status: false, message: "" });
   return config;
 });
-axios.interceptors.response.use((config) => {
-  setTimeout(() => {
+axios.interceptors.response.use(
+  (config) => {
+    setTimeout(() => {
+      store.commit("setLoading", false);
+    }, 500);
+    return config;
+  },
+  (e) => {
+    const { error } = e.response.data;
+    store.commit("setError", { status: true, message: error });
     store.commit("setLoading", false);
-  }, 500);
-  return config;
-}, (e) => {
-  const { error } = e.response.data;
-  store.commit("setError", {status: true, message: error});
-  store.commit("setLoading", false);
-  return Promise.reject(error);
-});
+    return Promise.reject(error);
+  }
+);
 const app = createApp(App);
 app.use(router);
 app.use(store);
