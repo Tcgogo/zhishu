559960d67614f07ee4dfbee48a16a7149e447274
Tcgogo <1812497229@qq.com>
2021-02-25 18:16:00 +0800

    项目目录结构优化

--- src/router.ts
+++ src/router.ts
@@ -1,58 +1,58 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import Signup from './views/Signup.vue'
-import ColumnDetail from './views/ColumnDetail.vue'
-import CreatePost from './views/CreatePost.vue'
-import PostDetail from './views/PostDetail.vue'
-import NotFound from './views/NotFound.vue'
-import store from './store'
-import { axios } from './libs/http'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Signup from "./views/SignUp.vue";
+import ColumnDetail from "./views/ColumnDetail.vue";
+import CreatePost from "./views/CreatePost.vue";
+import PostDetail from "./views/PostDetail.vue";
+import NotFound from "./views/NotFound.vue";
+import store from "./store";
+import { axios } from "./libs/http";
 
-const routerHistory = createWebHistory()
+const routerHistory = createWebHistory();
 const router = createRouter({
   history: routerHistory,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: Login,
-      meta: { redirectAlreadyLogin: true }
+      meta: { redirectAlreadyLogin: true },
     },
     {
-      path: '/signup',
-      name: 'signup',
+      path: "/signup",
+      name: "signup",
       component: Signup,
-      meta: { redirectAlreadyLogin: true }
+      meta: { redirectAlreadyLogin: true },
     },
     {
-      path: '/column/:id',
-      name: 'column',
-      component: ColumnDetail
+      path: "/column/:id",
+      name: "column",
+      component: ColumnDetail,
     },
     {
-      path: '/create',
-      name: 'create',
+      path: "/create",
+      name: "create",
       component: CreatePost,
-      meta: { requiredLogin: true }
+      meta: { requiredLogin: true },
     },
     {
-      path: '/posts/:id',
-      name: 'post',
-      component: PostDetail
+      path: "/posts/:id",
+      name: "post",
+      component: PostDetail,
     },
     {
-      path: '/*',
-      name: 'notFound',
-      components: NotFound
-    }
-  ]
-})
+      path: "/*",
+      name: "notFound",
+      components: NotFound,
+    },
+  ],
+});
 
 // 路由前置守卫
 router.beforeEach((to, from, next) => {
@@ -65,37 +65,40 @@ router.beforeEach((to, from, next) => {
   // } else {
   //   next()
   // }
-  const { user, token } = store.state
-  const { requiredLogin, redirectAlreadyLogin } = to.meta
+  const { user, token } = store.state;
+  const { requiredLogin, redirectAlreadyLogin } = to.meta;
   if (!user.isLogin) {
     // 没有登录
     if (token) {
-      axios.defaults.headers.common.Authorization = `Bearer ${token}`
-      store.dispatch('fetchCurrentUser').then(() => {
-        if (redirectAlreadyLogin) {
-          next('/')
-        } else {
-          next()
-        }
-      }).catch(err => {
-        console.log(err)
-        store.commit('logout')
-        next('login')
-      })
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+      store
+        .dispatch("fetchCurrentUser")
+        .then(() => {
+          if (redirectAlreadyLogin) {
+            next("/");
+          } else {
+            next();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          store.commit("logout");
+          next("login");
+        });
     } else {
       if (requiredLogin) {
-        next('login')
+        next("login");
       } else {
-        next()
+        next();
       }
     }
   } else {
     // 已经登录
     if (redirectAlreadyLogin) {
-      next('/')
+      next("/");
     } else {
-      next()
+      next();
     }
   }
-})
-export default router
+});
+export default router;
