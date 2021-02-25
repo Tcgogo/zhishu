14a83515744dfa789c8e408701efbec93bf010d9
Tcgogo <1812497229@qq.com>
2021-02-25 19:39:53 +0800

    项目目录结构优化

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="column-detail-page w-690">
-    <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
+    <div
+      class="column-info row mb-4 border-bottom pb-4 align-items-center"
+      v-if="column"
+    >
       <div class="col-3 text-center">
-        <img :src="column.avatar && column.avatar.fitUrl" :alt="column.title" class="rounded-circle border w-100">
+        <img
+          :src="column.avatar && column.avatar.fitUrl"
+          :alt="column.title"
+          class="rounded-circle border w-100"
+        />
       </div>
       <div class="col-9">
-        <h4>{{column.title}}</h4>
-        <p class="text-muted">{{column.description}}</p>
+        <h4>{{ column.title }}</h4>
+        <p class="text-muted">{{ column.description }}</p>
       </div>
     </div>
     <post-list :list="postList"></post-list>
@@ -14,76 +21,85 @@
       v-if="!isLastPage"
       @click="loadMorePage"
       class="btn btn-outline-primary mt-2 mb-5 mx-auto btn-block w-25 load-more"
-    >加载更多</button>
+    >
+      加载更多
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, onMounted, watch, reactive } from 'vue'
-import { useRoute } from 'vue-router'
-import { useStore } from 'vuex'
-import { GlobalDataProps, ColumnProps } from '../store/types'
-import PostList from '../components/PostList.vue'
-import { addColumnAvatar } from '../libs/helper'
-import useLoadMore from '../hooks/useLoadMore'
+import { defineComponent, computed, onMounted, watch, reactive } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import { GlobalDataProps, ColumnProps } from "../store/types";
+import PostList from "../components/PostList.vue";
+import { addColumnAvatar } from "../libs/helper";
+import useLoadMore from "../hooks/useLoadMore";
+import router from "@/router";
 
 type ColumnIdProps = string | undefined;
 
 export default defineComponent({
-  name: 'ColumnDetail',
+  name: "ColumnDetail",
   components: {
-    PostList
+    PostList,
   },
-  setup () {
-    const store = useStore<GlobalDataProps>()
-    const route = useRoute()
-    const currentId = route.params.id as ColumnIdProps
+  setup() {
+    const store = useStore<GlobalDataProps>();
+    const route = useRoute();
+    const currentId = route.params.id as ColumnIdProps;
 
     const loaded = reactive({
       currentPage: 0,
-      total: 0
-    })
-    const total = computed(() => loaded.total)
+      total: 0,
+    });
+    const total = computed(() => loaded.total);
     watch(store.state.posts.loadedColumns, () => {
-      console.log(store.state.posts.loadedColumns)
-      const { currentPage, total } = store.getters.getLoadedPost(currentId)
-      loaded.currentPage = currentPage
-      loaded.total = total
-    })
+      console.log(store.state.posts.loadedColumns);
+      const { currentPage, total } = store.getters.getLoadedPost(currentId);
+      loaded.currentPage = currentPage;
+      loaded.total = total;
+    });
+
+    watch(route, () => {
+      router.go(0);
+    });
 
     onMounted(() => {
-      store.dispatch('fetchColumn', currentId)
-      store.dispatch('fetchPosts', { columnId: currentId, pageSize: 3 })
-    })
+      store.dispatch("fetchColumn", currentId);
+      store.dispatch("fetchPosts", { columnId: currentId, pageSize: 3 });
+    });
 
     const column = computed(() => {
-      const selectColumn = store.getters.getColumnById(currentId) as ColumnProps | undefined
+      const selectColumn = store.getters.getColumnById(currentId) as
+        | ColumnProps
+        | undefined;
       if (selectColumn) {
-        addColumnAvatar(selectColumn, 100, 100)
+        addColumnAvatar(selectColumn, 100, 100);
       }
-      return selectColumn
-    })
+      return selectColumn;
+    });
 
     const params = {
       columnId: currentId,
       pageSize: 3,
-      currentPage: loaded.currentPage ? loaded.currentPage + 1 : 2
-    }
+      currentPage: loaded.currentPage ? loaded.currentPage + 1 : 2,
+    };
     const { loadMorePage, isLastPage } = useLoadMore(
-      'fetchPosts',
+      "fetchPosts",
       total,
       params
-    )
-    const postList = computed(() => store.getters.getPostsByCid(currentId))
+    );
+    const postList = computed(() => store.getters.getPostsByCid(currentId));
 
     return {
       column,
       postList,
       loadMorePage,
-      isLastPage
-    }
-  }
-})
+      isLastPage,
+    };
+  },
+});
 </script>
 
 <style scoped>
