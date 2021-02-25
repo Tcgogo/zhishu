e809b1d9b79e246a34ea76305f4123be7259ae1d
Tcgogo <68002136+Tcgogo@users.noreply.github.com>
2021-02-25 18:46:56 +0800

    Update proxy.js
    部署上线vercel

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/api')) {
-    target = 'http://api.vikingship.xyz/'
+    target = 'http://api.vikingship.xyz/api/'
   }
 
   // 创建代理对象并转发请求
